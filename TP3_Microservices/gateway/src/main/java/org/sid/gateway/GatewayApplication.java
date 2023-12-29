e223a5c88fcc3a29c4c5bad750f8bf565e331476
AZRHILILJihane <126695879+Azrhililjihane@users.noreply.github.com>
2023-12-29 17:42:48 +0100

    Add files via upload
    Architectures Micro-services avec Spring Cloud

---
+++ TP3_Microservices/gateway/src/main/java/org/sid/gateway/GatewayApplication.java
@@ -0,0 +1,56 @@
+package org.sid.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+     // Routage statique: Configuration de gateway en utlisant une classe java
+     /*@Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route((r)->r.path("/customers/**").uri("lb://customerService")) // lorque vous envoyer une req contacte le service qui a le nom "customerService"
+                .route((r)->r.path("/products/**").uri("lb://productService"))
+                .build();
+    }*/
+
+    // Routage dynamique: on doit mentionner le nom du service dans url
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties properties){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
